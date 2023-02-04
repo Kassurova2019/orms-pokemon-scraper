c138006369c891c9bf7097f787ce724a0eca0341
Luba Kassurova <kassurova@icloud.com>
2023-02-04 17:49:51 -0500

    commit tree 3998e7bd236b36d67049483a8f753e5bf82dca69

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+
+    attr_accessor :id, :name, :type, :db
+
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type
+        @db = db
+    end
+
+    def self.save(name,type,db)
+        db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?) ", name, type)
+    end
+
+    def self.find(id, db)
+        #db.execute("SELECT * FROM pokemon WHERE (id = ?)", id)
+        pokemon_1 = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
+        id_value = pokemon_1[0][0]
+        #puts "HEYYYYYYY IM HEEERRREEEEEEEEEE"
+
+        name_value = pokemon_1[0][1]
+        type_value = pokemon_1[0][2]
+
+
+        new_pokemon = Pokemon.new(id:id_value, name:name_value, type:type_value, db:db)
+        
+
+    end
 end
